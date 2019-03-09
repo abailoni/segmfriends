Fix some bugs in the masking of the gt ignore label

segmfriends/algorithms/WS/wsdt.py
@@ -111,28 +111,35 @@ class IntersectWithBoundaryPixels(object):
         self.offset_weights = offset_weights
         self.boundary_threshold = boundary_threshold
 
-    def __call__(self, affinities, segmentation):
+    def __call__(self, affinities, dtws_segm):
         hmap = from_affinities_to_hmap(affinities, self.offsets, self.used_offsets,
                                        self.offset_weights)
-        pixel_segm = np.arange(np.prod(segmentation.shape), dtype='uint64').reshape(segmentation.shape) + segmentation.max()
+        pixel_segm = np.arange(np.prod(dtws_segm.shape), dtype='uint64').reshape(dtws_segm.shape) + dtws_segm.max()
         boundary_mask = (1.-hmap) < self.boundary_threshold
-        new_segmentation = np.where(boundary_mask, pixel_segm, segmentation)
+
+        print("Relabel volume")
+        dtws_segm = vigra.analysis.labelVolume((dtws_segm * np.logical_not(boundary_mask)).astype('uint32'))
+
+        new_segmentation = np.where(boundary_mask, pixel_segm, dtws_segm)
+        print("Relabel consecutive")
         new_segmentation = vigra.analysis.relabelConsecutive(new_segmentation)[0]
-        new_segmentation = vigra.analysis.labelVolume(new_segmentation.astype('uint32'))
-        # print("Check new number of nodes!", new_segmentation.max())
+
+        print("Check new number of nodes!", new_segmentation.max())
 
         # from ... import vis as vis
         # import matplotlib.pyplot as plt
         #
-        # fig, ax = plt.subplots(ncols=2, nrows=1, figsize=(7, 7))
+        # fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(7, 7))
         # for a in fig.get_axes():
         #     a.axis('off')
         #
         # # affs_repr = np.linalg.norm(affs_repr, axis=-1)
         # # ax.imshow(affs_repr, interpolation="none")
         #
-        # vis.plot_gray_image(ax[0], hmap, z_slice=1)
-        # vis.plot_segm(ax[1], new_segmentation, z_slice=1, highlight_boundaries=False)
+        # vis.plot_gray_image(ax[0,0], hmap, z_slice=1)
+        # vis.plot_gray_image(ax[0, 1], boundary_mask.astype('float32'), z_slice=1)
+        # vis.plot_segm(ax[1,0], new_segmentation, z_slice=1, highlight_boundaries=False)
+        # vis.plot_segm(ax[1, 1], pixel_segm, z_slice=1, highlight_boundaries=False)
         #
         # pdf_path = "./hmap.pdf"
         # fig.savefig(pdf_path)
@@ -205,6 +212,28 @@ class WatershedOnDistanceTransformFromAffinities(WatershedOnDistanceTransform):
             assert foreground_mask.shape == segmentation.shape
             segmentation = segmentation.astype('int64')
             segmentation = np.where(foreground_mask, segmentation, np.ones_like(segmentation)*(-1))
+
+        if segmentation.max() > np.uint32(-1):
+            print("!!!!!!!!!WARNING!!!!!!!!!! uint32 limit reached!")
+
+
+        # from ... import vis as vis
+        # import matplotlib.pyplot as plt
+        #
+        # fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(7, 7))
+        # for a in fig.get_axes():
+        #     a.axis('off')
+        #
+        # # affs_repr = np.linalg.norm(affs_repr, axis=-1)
+        # # ax.imshow(affs_repr, interpolation="none")
+        #
+        # vis.plot_segm(ax[0], dtws, z_slice=1)
+        # vis.plot_segm(ax[1], intersect, z_slice=1, highlight_boundaries=False)
+        # vis.plot_segm(ax[2], segmentation, z_slice=1, highlight_boundaries=False)
+        #
+        # pdf_path = "./wsdt.pdf"
+        # fig.savefig(pdf_path)
+
 
         if self.return_hmap:
             return segmentation, hmap

segmfriends/algorithms/agglo/greedy_edge_contr.py
@@ -158,30 +158,6 @@ class GreedyEdgeContractionAgglomeraterFromSuperpixels(GreedyEdgeContractionAggl
         If the opposite is passed, set option `invert_affinities == True`
         """
 
-        # # ------------------------------------------
-        # import vigra
-        # # TODO: please move to the fragmenter!
-        # pixel_segm = np.arange(np.prod(segmentation.shape), dtype='uint64').reshape(segmentation.shape) + segmentation.max()
-        # # Full CREMI:
-        # mask1 = (affinities[[0,1,2,4,5]] < 0.3).max(axis=0)
-        # mask2 = (affinities[[7,8]] < 0.04).max(axis=0)
-        # # Small noise experiments:
-        # # mask1 = (affinities[[0, 1, 2, 4, 5]] < 0.6).max(axis=0)
-        # # mask2 = (affinities[[7, 8, 10, 11]] < 0.2).max(axis=0)
-        # affinities_mask = np.logical_or(mask1, mask2)
-        # # affinities_mask = mask1
-        # new_segmentation = np.where(affinities_mask, pixel_segm, segmentation)
-        # new_segmentation = vigra.analysis.relabelConsecutive(new_segmentation)[0]
-        # new_segmentation = vigra.analysis.labelVolume(new_segmentation.astype('uint32'))
-        # print("Check new number of nodes!", new_segmentation.max())
-        #
-        # #
-        # # vigra.writeHDF5(new_segmentation.astype('uint64'), '/home/abailoni_local/hci_home/temp.h5', 'new_segm')
-        #
-        # segmentation = new_segmentation
-        # # vigra.writeHDF5(segmentation.astype('uint64'), '/home/abailoni_local/hci_home/temp.h5', 'segm')
-        # # ------------------------------------------
-
         tick = time.time()
         # TODO: I think I should really consider implementing the max and sum in the statistics...
         featurer_outputs = self.featurer(affinities, segmentation)
@@ -225,7 +201,7 @@ class GreedyEdgeContractionAgglomeraterFromSuperpixels(GreedyEdgeContractionAggl
                                           node_sizes=node_sizes,
                                           is_merge_edge=is_local_edge,
                                           return_UCM=self.return_UCM,
-                                          return_agglomeration_data=True,
+                                          return_agglomeration_data=False,
                                           **self.extra_aggl_kwargs,
                                           **self.extra_runAggl_kwargs)
 
@@ -233,11 +209,17 @@ class GreedyEdgeContractionAgglomeraterFromSuperpixels(GreedyEdgeContractionAggl
             print("Took {} s!".format(out_dict["runtime"]))
             print("Getting final segm...")
 
+        # ignore_mask = segmentation == -1
         final_segm = mappings.map_features_to_label_array(
             segmentation,
             np.expand_dims(node_labels, axis=-1),
-            number_of_threads=self.n_threads
+            number_of_threads=self.n_threads,
+            fill_value=-1.,
+            ignore_label=-1,
         )[..., 0]
+        # Increase by one, so ignore label becomes 0:
+        final_segm += 1
+
 
         # Compute MC energy:
         edge_labels = graph.nodesLabelsToEdgeLabels(node_labels)
@@ -373,7 +355,7 @@ class GreedyEdgeContractionAgglomerater(GreedyEdgeContractionAgglomeraterBase):
                                           edge_sizes=edge_sizes,
                                           is_merge_edge=is_local_edge,
                                          return_UCM=self.return_UCM,
-                                          return_agglomeration_data=True,
+                                          return_agglomeration_data=False,
                                           ignored_edge_weights=ignored_edge_weights,
                                           **self.extra_aggl_kwargs)
 
@@ -396,6 +378,7 @@ class GreedyEdgeContractionAgglomerater(GreedyEdgeContractionAgglomeraterBase):
             print("MC energy: {}".format(MC_energy))
             print("Agglomerated in {} s".format(out_dict['runtime']))
 
+        # TODO: map ignore label -1 to 0!
         segmentation = nodeSeg.reshape(image_shape)
 
         out_dict['MC_energy'] = MC_energy
@@ -426,7 +409,7 @@ def runGreedyGraphEdgeContraction(
     every edge.
     """
 
-    if update_rule == 'MutexWatershed' and False:
+    if update_rule == 'MutexWatershed':
         assert not return_UCM
         # In this case we use the efficient MWS clustering implementation in affogato:
         nb_nodes = graph.numberOfNodes

segmfriends/vis/base.py
@@ -10,7 +10,7 @@ from ..transform.segm_to_bound import compute_boundary_mask_from_label_image
 
 
 # Build random color map:
-__MAX_LABEL__ = 1000000
+__MAX_LABEL__ = 100000000
 rand_cm = matplotlib.colors.ListedColormap(np.random.rand(__MAX_LABEL__, 3))
 
 